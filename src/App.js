e284c3c5e2f45e44c6337ac7f95fdc749880a5e8
kipkorirgideon <korirgideon29@gmail.com>
2021-06-07 14:07:08 +0300

    MAIN

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="App">
       <h1>hello people</h1>
+      <h2>From the origin master</h2>
+      <l1>Python</l1>
     </div>
   );
 }
 
 export default App;
+ 
